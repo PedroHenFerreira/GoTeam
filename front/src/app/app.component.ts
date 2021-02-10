a3825ec1448e1243380492ef2259d794cc1a523d
Bernardo Ventura <bezin888@gmail.com>
2021-02-10 13:34:30 -0300

    + termina tab-menu e side-menu

---
+++ front/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Home', url: '/home', icon: 'game-controller' },
+    { title: 'Perfil', url: '/profile', icon: 'person' },
+    { title: 'Evento', url: '/evento', icon: 'calendar-clear' },
+    { title: 'Criar um evento', url: '/criar_evento', icon: 'trophy' },
+    { title: 'Pesquisar', url: '/pesquisar', icon: 'search' },
+    { title: 'Configurações', url: '/configuracoes', icon: 'settings' },
+    { title: 'Sair', url: '/sair', icon: 'log-out' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
+}
