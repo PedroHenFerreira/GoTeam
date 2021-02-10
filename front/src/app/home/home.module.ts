9fdf0196c955806ba5ca544de8edbfe7968fb6d7
PedroPandaFerreira <ph.ferreira619@gmail.com>
2021-02-10 18:01:18 -0300

    +home-page

---
+++ front/src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+import { HomePage } from './home.page';
+
+import { HeaderComponent } from '../components/header/header.component';
+import { CardEventoDestaqueComponent } from '../components/card-evento-destaque/card-evento-destaque.component'
+import { CardPostComponent } from '../components/card-post/card-post.component'
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage, HeaderComponent, CardEventoDestaqueComponent, CardPostComponent]
+})
+export class HomePageModule {}
