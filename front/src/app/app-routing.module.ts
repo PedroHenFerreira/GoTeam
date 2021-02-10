a56775b29646376e0245a1ad874298b1d6bbc3ed
PedroPandaFerreira <ph.ferreira619@gmail.com>
2021-02-10 18:22:55 -0300

    +goteam-imagens-index.html-variables.scss

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -10,7 +10,15 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
+
 ];
 
 @NgModule({
