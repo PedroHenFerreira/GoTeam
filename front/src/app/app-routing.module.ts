40766beb328ef46b61d110012733e2f7f84752ad
PedroPandaFerreira <ph.ferreira619@gmail.com>
2021-02-15 19:01:25 -0300

    +criar-post

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -34,6 +34,10 @@ const routes: Routes = [
   {
     path: 'editar-perfil',
     loadChildren: () => import('./editar-perfil/editar-perfil.module').then( m => m.EditarPerfilPageModule)
+  },
+  {
+    path: 'criar-post',
+    loadChildren: () => import('./criar-post/criar-post.module').then( m => m.CriarPostPageModule)
   }
 
 
